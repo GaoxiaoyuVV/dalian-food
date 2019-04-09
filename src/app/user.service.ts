8169f3533670c6dc2c261c6d990f63bd838d17c2
Vasquez Gao -X (xiaoygao) <xiaoygao@cisco.com>
2019-04-09 13:51:07 +0800

    modify format

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,16 +6,16 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class UserService {
-private userUrl='/api/auth';
-  constructor(private http:HttpClient,
-    private router:Router) { }
-public login(username: string, password: string) {
-      /* console.log('in userservice',`${this.userUrl}/login`) */
-       return this.http.post(`${this.userUrl}`, {
-         'username': username,
-         'password': password
-       });
- }
+  private userUrl = '/api/auth';
+  constructor(private http: HttpClient,
+    private router: Router) { }
+  public login(username: string, password: string) {
+    /* console.log('in userservice',`${this.userUrl}/login`) */
+    return this.http.post(`${this.userUrl}`, {
+      'username': username,
+      'password': password
+    });
+  }
   public register(username: string, password: string, tel: string) {
     return this.http.post('/api/auth/register', {
       'username': username,
@@ -24,46 +24,46 @@ public login(username: string, password: string) {
       'role': 'user'
     })
   }
-/*  public geta(name:string){
-return this.http.post('api/auth/searchtrue',{
-  'name':name
-})
- } */
-public search(input:string){
-return this.http.post('/api/auth/search',{
-  'input':input
-})
-}
-HotHighchart():Observable<any>{
-  return this.http.get<any>('/api/auth/hcharts');
-}
-PriceHighchart():Observable<any>{
-  return this.http.get<any>('/api/auth/hcharts1');
-}
-CommentHighchart():Observable<any>{
-  return this.http.get<any>('/api/auth/hcharts2');
-}
-public getallinfo(searchthing:string){
+  /*  public geta(name:string){
   return this.http.post('api/auth/searchtrue',{
-'searchthing':searchthing,
-  }) 
-}
- /* }
- uploadFile( file: File): Observable<HttpEvent<any>> {
-
-  let formData = new FormData();
-  formData.append('upload', file);
-
-  let params = new HttpParams();
-console.log(file)
-  const options = {
-    params: params,
-    reportProgress: true,
-  
-  };
-
-  const req = new HttpRequest('POST', '/api/auth/upload', formData, options);
-  return this.http.request(req);
-} */
+    'name':name
+  })
+   } */
+  public search(input: string) {
+    return this.http.post('/api/auth/search', {
+      'input': input
+    })
+  }
+  HotHighchart(): Observable<any> {
+    return this.http.get<any>('/api/auth/hcharts');
+  }
+  PriceHighchart(): Observable<any> {
+    return this.http.get<any>('/api/auth/hcharts1');
+  }
+  CommentHighchart(): Observable<any> {
+    return this.http.get<any>('/api/auth/hcharts2');
+  }
+  public getallinfo(searchthing: string) {
+    return this.http.post('api/auth/searchtrue', {
+      'searchthing': searchthing,
+    })
+  }
+  /* }
+  uploadFile( file: File): Observable<HttpEvent<any>> {
+ 
+   let formData = new FormData();
+   formData.append('upload', file);
+ 
+   let params = new HttpParams();
+ console.log(file)
+   const options = {
+     params: params,
+     reportProgress: true,
+   
+   };
+ 
+   const req = new HttpRequest('POST', '/api/auth/upload', formData, options);
+   return this.http.request(req);
+ } */
 
 }
